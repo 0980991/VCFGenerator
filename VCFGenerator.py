f33ae7a09d3643d55197d0fa62a05722c33efe7c
Maurice Monster <288441@mac.home>
2026-04-10 16:16:16 +0200

    Added CSV support
     - Import csv file with any degree of field population
    
     - Other contact information can be added including:
    
       - name
    
       - organization
    
       - title
    
       - email
    
       - address

--- VCFGenerator.py
+++ VCFGenerator.py
@@ -1,5 +1,6 @@
 import re
 from datetime import datetime
+import csv
 
 class VCFGenerator:
     def __init__(self, input_path, output_path=""):
@@ -9,7 +10,7 @@ class VCFGenerator:
         else:
             self.output_file = output_path
         
-    def cleanNumber(self, number):
+    def sanitizeNumber(self, number):
         # Keep digits and leading +
         number = number.strip()
         if number.startswith("+"):
@@ -24,18 +25,51 @@ class VCFGenerator:
         # Using the built-in open() function with strings
         with open(self.input_file, "r", encoding="utf-8") as f, \
             open(self.output_file, "w", encoding="utf-8") as out:
-            
-            for i, line in enumerate(f, start=1):
-                number = self.cleanNumber(line)
-                if not number:
-                    continue
+                
+            # Parse txt file
+            if self.input_file.endswith(".txt"): 
+                for i, line in enumerate(f, start=1):
+                    number = self.sanitizeNumber(line)
+                    if not number:
+                        continue
 
-                name = f"Contact {i:03d}"
-                out.write(
-                    "BEGIN:VCARD\n"
-                    "VERSION:3.0\n"
-                    f"N:{name};{name};;;\n"
-                    f"FN:{name}\n"
-                    f"TEL;TYPE=CELL:{number}\n"
-                    "END:VCARD\n\n"
-                )
+                    name = f"Contact {i:04d}"
+                    out.write(
+                        "BEGIN:VCARD\n"
+                        "VERSION:3.0\n"
+                        f"N:{name};{name};;;\n"
+                        f"FN:{name}\n"
+                        f"TEL;TYPE=CELL:{number}\n"
+                        "END:VCARD\n\n"
+                    )
+
+            # Parse csv file
+            elif self.input_file.endswith(".csv"):
+                reader = csv.reader(f)
+                contact_data = list(reader)
+                
+                for i, contact in enumerate(contact_data):
+                    # When there is nothing in the "Phone" field of the contact record, the contact will not be created
+                    if i != 0 and contact[0] == "":
+                        continue
+                    
+                    sanitized_number, name, organization, title, email, address = self.sanitizeNumber(contact[0]), contact[1], contact[2], contact[3], contact[4], contact[5]
+                    if not name:
+                        name = f"Contact {i:03d}"
+                    try: 
+                        first, last = name.split()
+                    except ValueError: 
+                        first, last = name, ""
+                    
+                    out.write(
+                        "BEGIN:VCARD\n"
+                        "VERSION:3.0\n"
+                        f"FN:{first} {last}\n"
+                        f"N:{last};{first}\n"
+                        f"ORG:{organization}\n"
+                        f"TITLE:={title}\n"
+                        f"TEL;TYPE=CELL:{sanitized_number}\n"
+                        f"EMAIL:{email}\n"
+                        f"ADR:{address}\n"
+                        "END:VCARD\n\n"
+                    )
